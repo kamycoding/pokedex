8d6163cc5d6c7d64a8b449d08d8bf66669439724
KamyCoding <kamyar.zamanfar@gmail.com>
2026-06-13 22:28:29 +0200

    feat: add DOM element references

---
+++ js/dom.js
@@ -0,0 +1,13 @@
+export const dom = {
+  pokemonGrid: document.getElementById("pokemon-grid"),
+  searchForm: document.querySelector(".search-form"),
+  searchInput: document.querySelector('[data-id="search-input"]'),
+  searchButton: document.querySelector('[data-id="search-button"]'),
+  notFoundMessage: document.querySelector('[data-id="not-found"]'),
+  loadMoreButton: document.querySelector('[data-id="load-more-button"]'),
+  dialog: document.querySelector('[data-id="dialog"]'),
+  dialogContent: document.querySelector('[data-id="overlay-pokemon-name"]'),
+  closeDialogButton: document.querySelector('[data-id="close-dialog-button"]'),
+  previousButton: document.querySelector('[data-id="prev-button"]'),
+  nextButton: document.querySelector('[data-id="next-button"]'),
+};
