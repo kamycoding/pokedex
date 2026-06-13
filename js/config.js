e02469cd96232333603b6011f362aac51e44cfdf
KamyCoding <kamyar.zamanfar@gmail.com>
2026-06-13 22:49:44 +0200

    fix: correct Pokemon config constant names

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = "https://pokeapi.co/api/v2";
-export const POKEMAN_BATCH_SIZE = 20;
-export const MAX_POKEMAN_COUNT = 151;
+export const POKEMON_BATCH_SIZE = 20;
+export const MAX_POKEMON_COUNT = 151;
 export const MIN_SEARCH_LENGTH = 3;
